63228d89bf0460bcc61561589e8bafb765b7404b
uxert <uxert@wp.pl>
2025-05-02 22:49:49 +0200

    Create basic form for signup.
    Make the app accept POST request from /register and add basic skeleton of processing (which of course cannot be complete without a database) of register requests.

--- TaskManager/website/auth.py
+++ TaskManager/website/auth.py
@@ -1,14 +1,63 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from typing import NamedTuple
+
+
+class SimpleResponse(NamedTuple):
+    """ Simple NamedTuple that will allow to pass results from database functions - status and error explanation"""
+    success: bool
+    message: str = ''
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login")
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    username = request.form["username"]
+    password = request.form["password"]
 
-@auth.route("/register")
+
+
+def validate_signup(email, username, password1, password2) -> bool:
+    if password1 != password2:
+        flash("Passwords don't match!", category="error")
+        return False
+    if len(username) <= 3:
+        flash("Username too short! Should be at least 4 characters", category="error")
+        return False
+    if len(password1) <=7: #passwords are equal at this point
+        flash("Password too short! Should be at least 8 characters", category="error")
+        return False
+    # check if either email or username already used
+    if False:
+        flash(f"Username {username} is already taken!", category="error")
+        return False
+    if False:
+        flash(f"Account registered on {email} already exists!", category="error")
+        return False
+    return True
+
+def register_user(email, username, password) -> SimpleResponse:
+    """Function to register a new user in the database."""
+    return SimpleResponse(True)
+
+@auth.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    email = request.form["email"]
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if validate_signup(email, username, password1, password2) is False:
+        return render_template("register.html")
+
+    result = register_user(email, username, password1)
+    if result.success is True:
+        flash("Account registered successfully!", category="success")
+    if result.success is not True:
+        flash(f"Account was not registered: {result.message}", category="error")
     return render_template("register.html")
 
 @auth.route("/logout")
