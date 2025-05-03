3a13eea6b073bade60777ad556fce84f19a80b5b
uxert <uxert@wp.pl>
2025-05-03 12:05:12 +0200

    Create database and define models

--- TaskManager/website/__init__.py
+++ TaskManager/website/__init__.py
@@ -1,14 +1,32 @@
 from flask import Flask
 from .views import views
 from.auth import auth
+from flask_sqlalchemy import SQLAlchemy
+from os import path
 
+WEBSITE_DIR = path.dirname(path.abspath(__file__))
+BACKEND_DIR = path.dirname(WEBSITE_DIR)
+
+db = SQLAlchemy()
+DATABASE_NAME = "database.db"
+DATABASE_PATH = path.join(BACKEND_DIR, DATABASE_NAME)
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "<KEY>"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DATABASE_PATH}"
+    db.init_app(app)
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import User, Task  # so that these classes are actually  defined when creating the database
+    create_database(app)
+
     return app
 
+def create_database(app: Flask):
+    if not path.exists(DATABASE_PATH):
+        with app.app_context():
+            db.create_all()
+        print(f"Created database at {DATABASE_PATH}!")
---
+++ TaskManager/website/models.py
@@ -0,0 +1,38 @@
+"""Models for database"""
+from . import db
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    tasks = db.relationship("Task")
+
+    def __init__(self, email, username, password, **kwargs):
+        super(User, self).__init__(**kwargs)
+        self.email = email
+        self.username = username
+        self.set_password(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password, method="scrypt")
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    importance = db.Column(db.Integer, nullable=False)
+    deadline = db.Column(db.Date, nullable=False)
+    est_time_days = db.Column(db.Integer, nullable=True)
+    description = db.Column(db.String(2000), nullable=True)
+    user_id = db.Column(db.ForeignKey("user.id"), nullable=False)
+    parent_task_id = db.Column(db.ForeignKey("task.id"), nullable=True)
+    children = db.relationship(
+        "Task",
+        backref=db.backref("parent", remote_side=[id]),
+        lazy="select"
+    )
