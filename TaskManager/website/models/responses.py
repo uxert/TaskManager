3df04fa5435b3f2b9f04208d2cfc9c0494e8e412
uxert <uxert@wp.pl>
2025-05-11 13:11:21 +0200

    Change SimpleResponse from NamedTuple to a dataclass. Add ManyTasksResponse.
    Dataclasses are much more practical when it comes to extending, like the ManyTasksResponse does

--- TaskManager/website/models/responses.py
+++ TaskManager/website/models/responses.py
@@ -1,14 +1,23 @@
 """This file is meant to contain various responses available throughout the project.
 One may be responses from API, other internal server messages - we will see"""
-from typing import NamedTuple
+from dataclasses import dataclass
 from pydantic import BaseModel
 
 
-class SimpleResponse(NamedTuple):
-    """ Simple NamedTuple that will allow to pass results from database functions - status and error explanation"""
+@dataclass(frozen=True)
+class SimpleResponse:
+    """ Simple dataclass that will allow to pass results from database functions - status and error explanation"""
     success: bool
     message: str = ''
     exception: Exception = None
+
+@dataclass(frozen=True)
+class ManyTasksResponse(SimpleResponse):
+    """
+    Inherits from SimpleResponse and extends it by one additional field - tasks. It's a list of dicts for easy JSON
+    serializaiton
+    """
+    tasks: list[dict] = None
 
 class TerminalAPIResponse(BaseModel):
     status: str
