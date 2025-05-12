b922629e3c4d4ebda5aca816a66e228c768c1410
uxert <uxert@wp.pl>
2025-05-12 10:59:50 +0200

    Implement edit operation on db

--- TaskManager/website/db_operations.py
+++ TaskManager/website/db_operations.py
@@ -1,5 +1,5 @@
 from . import db
-from .models import AddTaskRequestModel
+from .models import AddTaskRequestModel, EditTaskRequestModel
 from .models.responses import SimpleResponse, ManyTasksResponse, OneTaskResponse
 from .db_models import Task
 from typing import Optional
@@ -75,3 +75,36 @@ def try_removing_specific_task(user_id, task_id) -> SimpleResponse:
         return OneTaskResponse(False, f'No task with id {task_id} found in your account', nrf)
     except SQLAlchemyError as e:
         return OneTaskResponse(False, str(e), e)
+
+def try_editing_specific_task(user_id, edit_request: EditTaskRequestModel) -> SimpleResponse:
+    if not isinstance(edit_request, EditTaskRequestModel):
+        err = TypeError("Provided edit_request is not instance of EditTaskRequestModel")
+        return SimpleResponse(False, str(err), err)
+    if not isinstance(user_id, int):
+        err = TypeError("Provided user_id is not an instance of int")
+        return SimpleResponse(False, str(err), err)
+
+    # get from database
+    task_id = edit_request.task_id
+    try:
+        task = Task.query.filter_by(user_id=user_id, id=task_id).one()
+    except NoResultFound as nrf:
+        return SimpleResponse(False, f'Cannot edit: no task with id {task_id} found in your account', nrf)
+    except SQLAlchemyError as e:
+        return SimpleResponse(False, str(e), e)
+
+    # edit
+    try:
+        task.title = edit_request.title
+        task.importance = edit_request.importance
+        task.deadline = edit_request.deadline
+        task.description = edit_request.description
+        task.est_time_days = edit_request.est_time_days
+        db.session.commit()
+
+        return SimpleResponse(True, message=f"successfully edited task with id {task.id}")
+    except SQLAlchemyError as e:
+        return OneTaskResponse(False, str(e), e)
+
+
+
